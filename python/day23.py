78e13e0e99038f24004eb446508d887f44386953
Will Killian <william.killian@outlook.com>
2023-12-23 19:16:13 -0500

    Update day 23
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- python/day23.py
+++ python/day23.py
@@ -1,49 +1,51 @@
 from collections import defaultdict
 
+Point = tuple[int, int]
+
 
 def make_graph(
     data: list[str], *, part2: bool = False
-) -> defaultdict[tuple[int, int], set[tuple[int, int, int]]]:
-    adj: defaultdict[tuple[int, int], set[tuple[int, int, int]]] = defaultdict(set)
+) -> defaultdict[Point, set[tuple[Point, int]]]:
+    adj: defaultdict[Point, set[tuple[Point, int]]] = defaultdict(set)
     for r, row in enumerate(data):
         for c, v in enumerate(row):
+            loc: Point = (r, c)
             if not part2:
                 match v:
                     case ".":
-                        for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+                        for dr, dc in ((-1, 0), (0, -1), (0, 1), (1, 0)):
                             if (
                                 (0 <= (ar := r + dr) < len(data))
                                 and (0 <= (ac := c + dc) < len(row))
                                 and data[ar][ac] == "."
                             ):
-                                adj[(r, c)].add((ar, ac, 1))
-                                adj[(ar, ac)].add((r, c, 1))
+                                new_loc: Point = (ar, ac)
+                                adj[loc].add((new_loc, 1))
+                                adj[new_loc].add((loc, 1))
                     case ">":
-                        adj[(r, c)].add((r, c + 1, 1))
-                        adj[(r, c - 1)].add((r, c, 1))
+                        adj[loc].add(((r, c + 1), 1))
+                        adj[(r, c - 1)].add((loc, 1))
                     case "v":
-                        adj[(r, c)].add((r + 1, c, 1))
-                        adj[(r - 1, c)].add((r, c, 1))
-            elif v in ".>v":
-                for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+                        adj[loc].add(((r + 1, c), 1))
+                        adj[(r - 1, c)].add((loc, 1))
+            elif v != "#":
+                for dr, dc in ((-1, 0), (0, -1), (0, 1), (1, 0)):
                     if (0 <= (ar := r + dr) < len(data)) and (
                         0 <= (ac := c + dc) < len(row)
                     ):
-                        if data[ar][ac] in ".>v":
-                            adj[(r, c)].add((ar, ac, 1))
-                            adj[(ar, ac)].add((r, c, 1))
+                        new_loc = (ar, ac)
+                        if data[ar][ac] != "#":
+                            adj[loc].add((new_loc, 1))
+                            adj[new_loc].add((loc, 1))
     if part2:
         while True:
             for n, e in adj.items():
                 if len(e) == 2:
-                    a, b = e
-                    nx, ny = n
-                    ax, ay, al = a
-                    bx, by, bl = b
-                    adj[(ax, ay)].remove((nx, ny, al))
-                    adj[(bx, by)].remove((nx, ny, bl))
-                    adj[(ax, ay)].add((bx, by, al + bl))
-                    adj[(bx, by)].add((ax, ay, al + bl))
+                    (aloc, al), (bloc, bl) = e
+                    adj[aloc].remove((n, al))
+                    adj[bloc].remove((n, bl))
+                    adj[aloc].add((bloc, al + bl))
+                    adj[bloc].add((aloc, al + bl))
                     del adj[n]
                     break
             else:
@@ -52,27 +54,29 @@ def make_graph(
 
 
 def solve(
-    graph: defaultdict[tuple[int, int], set[tuple[int, int, int]]],
-    goal: tuple[int, int],
+    graph: defaultdict[Point, set[tuple[Point, int]]],
+    start: Point,
+    goal: Point,
 ) -> int:
-    q: list[tuple[int, int, int]] = [(0, 1, 0)]
-    visited: set[tuple[int, int]] = set()
+    q: list[tuple[Point, int]] = [(start, 0)]
+    visited: defaultdict[Point, bool] = defaultdict(bool)
     longest: int = 0
     while q:
-        match q.pop():
-            case r, c, -1:
-                visited.remove((r, c))
-            case r, c, d if (r, c) == goal:
-                longest = max(d, longest)
-            case r, c, d if (r, c) not in visited:
-                visited.add((r, c))
-                q.append((r, c, -1))
-                q.extend((ar, ac, d + l) for ar, ac, l in graph[(r, c)])
+        loc, d = q.pop()
+        if d == -1:
+            visited[loc] = False
+        elif loc == goal:
+            longest = max(d, longest)
+        elif not visited[loc]:
+            visited[loc] = True
+            q.append((loc, -1))
+            q.extend((new_loc, d + l) for new_loc, l in graph[loc])
     return longest
 
 
 with open("inputs/day23.txt") as f:
     data = list(map(str.rstrip, f.readlines()))
-    goal: tuple[int, int] = len(data) - 1, len(data[0]) - 2
-    print(solve(make_graph(data, part2=False), goal))
-    print(solve(make_graph(data, part2=True), goal))
+    start: Point = (0, 1)
+    goal: Point = len(data) - 1, len(data[0]) - 2
+    print(solve(make_graph(data, part2=False), start, goal))
+    print(solve(make_graph(data, part2=True), start, goal))
